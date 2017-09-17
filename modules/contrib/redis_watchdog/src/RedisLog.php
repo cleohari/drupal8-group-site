073257f95d3277ec9d3dd1cbbb90300cc80cdf94
Brady Owens <info@fastglass.net>
2017-09-17 18:56:45 -0400

    PDS-18 Correcting namespace and method usage in the RedisLog class

--- modules/contrib/redis_watchdog/src/RedisLog.php
+++ modules/contrib/redis_watchdog/src/RedisLog.php
@@ -1,6 +1,8 @@
 <?php
 
-namespace Drupal\redis_watchdog\redis;
+namespace Drupal\redis_watchdog;
+use Drupal\redis;
+use Drupal\redis\ClientFactory as RedisClient;
 
 /**
  * Class RedisLog
@@ -19,8 +21,10 @@ class RedisLog {
 
   protected $archivelimit;
 
-  public function __construct($prefix = '', $recentlength = 100, $archivelimit = 5000) {
-    $this->client = Redis_Client::getManager()->getClient();
+  public function __construct($prefix = '', $recentlength = 200, $archivelimit = 5000) {
+    // @todo remove this when converstion to Drupal 8 is finished.
+    // $this->client = Redis_Client::getManager()->getClient();
+    $this->client = RedisClient::getClient();
     if (!empty($prefix)) {
       $this->key = 'drupal:watchdog:' . $prefix . ':';
     }
