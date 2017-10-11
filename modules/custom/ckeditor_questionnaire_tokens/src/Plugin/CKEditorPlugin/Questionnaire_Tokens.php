a7da6669781e263206fee6ec6cc8a2dfca172dac
Scott Weiss <scott@fastglass.net>
2017-10-11 09:04:29 -0500

    PDS-64 - Initial commit of base module structure.

---
+++ modules/custom/ckeditor_questionnaire_tokens/src/Plugin/CKEditorPlugin/Questionnaire_Tokens.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Drupal\ckeditor_questionnaire_tokens\Plugin\CKEditorPlugin;
+
+use Drupal\ckeditor\CKEditorPluginBase;
+use Drupal\editor\Entity\Editor;
+
+/**
+ * Defines the "questionnaire_tokens" plugin.
+ *
+ * NOTE: The plugin ID ('id' key) corresponds to the CKEditor plugin name.
+ * It is the first argument of the CKEDITOR.plugins.add() function in the
+ * plugin.js file.
+ *
+ * @CKEditorPlugin(
+ *   id = "questionnaire_tokens",
+ *   label = @Translation("Questionnaire Tokens")
+ * )
+ */
+class Questionnaire_Tokens extends CKEditorPluginBase {
+
+
+  /**
+   * {@inheritdoc}
+   *
+   * NOTE: The keys of the returned array corresponds to the CKEditor button
+   * names. They are the first argument of the editor.ui.addButton() or
+   * editor.ui.addRichCombo() functions in the plugin.js file.
+   */
+  public function getButtons() {
+    // Make sure that the path to the image matches the file structure of
+    // the CKEditor plugin you are implementing.
+    return [
+      'Questionnaire_tokens' => [
+        'label' => t('Questionnaire Tokens'),
+        'image' => base_path() . 'libraries/questionnaire_tokens/icons/token.png',
+      ],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFile() {
+    // Make sure that the path to the plugin.js matches the file structure of
+    // the CKEditor plugin you are implementing.
+    return base_path() . 'libraries/questionnaire_tokens/plugin.js';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isInternal() {
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDependencies(Editor $editor) {
+    return [];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getLibraries(Editor $editor) {
+    return [];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConfig(Editor $editor) {
+    return [];
+  }
+
+}
