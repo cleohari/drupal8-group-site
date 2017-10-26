125194cc38f4d9b4518d9d535e5bc0bc32f507b7
Scott Weiss <scott@fastglass.net>
2017-10-26 12:27:28 -0500

    PDS-64 - Initial commit of the Placeholder CK Editor plugin that has been modified to display a dynamically built array in a drop down list.

--- libraries/placeholder/dialogs/placeholder.js
+++ libraries/placeholder/dialogs/placeholder.js
@@ -16,6 +16,41 @@ CKEDITOR.dialog.add( 'placeholder', function( editor ) {
 		generalLabel = editor.lang.common.generalTab,
 		validNameRegex = /^[^\[\]<>]+$/;
 
+	//var temparray = [ ['Company'], ['Email'], ['First Name'], ['Last Name'],['This guy'] ];
+  //var temparray = [ 'Company', 'Email', 'First Name', 'Last Name','This guy' ];
+	var myString = document.getElementById('available_tokens').innerText;
+	var temparray = myString.split(",");
+
+  var arr = new Array();
+  //arr[0] = [];
+  for (var i = 0; i < temparray.length; i++) {
+    alert(temparray[i]);
+    arr[i] = new Array();
+    arr[i][0] = temparray[i];
+    //Do something
+  }
+
+	/*var arr = [];
+	arr[0] = [];
+	arr[0][0] = temparray[0];
+  arr[0][1] = temparray[1];
+  arr[0][2] = temparray[2];*/
+	/*for(var i=0;i<3;++i){
+    arr[0][i] = temparray[i];
+		//alert(temparray[i]);
+	}*/
+	//alert(arr);
+	//arr[0] = temparray;
+	// for(var i=0; i< 1;i++)
+	// {
+	// 	var columns=[];
+	// 	for(var j=0; j<3; j++)
+	// 	{
+	//
+	// 	}
+	// 	arr[i]
+	// }
+	alert(arr);
 	return {
 		title: lang.title,
 		minWidth: 300,
@@ -29,8 +64,10 @@ CKEDITOR.dialog.add( 'placeholder', function( editor ) {
 					// Dialog window UI elements.
 					{
 						id: 'name',
-						type: 'text',
-						style: 'width: 100%;',
+						//type: 'text',
+            type: 'select',
+            items: arr,
+						style: 'width: 400px;',
 						label: lang.name,
 						'default': '',
 						required: true,
