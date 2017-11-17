37aef782abb0805e06d55236e983654bbd5f8b4e
Brady Owens <info@fastglass.net>
2017-11-16 22:16:00 -0500

    PDS-77 Change in routing.

--- modules/custom/pds_create_node_from_webform/src/Controller/CreateNodeController.php
+++ modules/custom/pds_create_node_from_webform/src/Controller/CreateNodeController.php
@@ -3,22 +3,47 @@
 namespace Drupal\pds_create_node_from_webform\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-
+use Drupal\Core\Session\AccountInterface;
+use Drupal\group\GroupMembershipLoader;
 /**
  * Class CreateNodeController.
  */
 class CreateNodeController extends ControllerBase {
 
   /**
+   * Current user.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  private $currentUser;
+
+  /**
+   * Constructs a new CreateNodeController.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $current_user
+   *   The current user.
+   */
+  public function __construct(AccountInterface $current_user) {
+    $this->currentUser = $current_user;
+  }
+
+
+  /**
    * Createnode.
    *
-   * @return string
+   * @return array
    *   Return Hello string.
    */
-  public function createNode() {
+  public function createNodePage() {
+    $groupMemmberShip = new GroupMembershipLoader();
+
+
+
+
+
     return [
       '#type' => 'markup',
-      '#markup' => $this->t('Implement method: createNode')
+      '#markup' => $this->t('Implement method: createNode'),
     ];
   }
 
