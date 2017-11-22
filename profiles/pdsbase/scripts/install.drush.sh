fdcf4a6736db82e6a855ef3e3932540d50593f03
Scott Weiss <scott@fastglass.net>
2017-11-22 12:42:49 -0600

    PDS-84 - Adding the MTT Enterprise Plus theme main navigation menu.

--- profiles/pdsbase/scripts/install.drush.sh
+++ profiles/pdsbase/scripts/install.drush.sh
@@ -124,7 +124,11 @@ echo "$REDIS_SETTINGS" >> ${DWD}sites/default/settings.php
 chmod 444 ${DWD}sites/default/settings.php
 #chmod 555 ${DWD}sites/default
 
+#Set the theme to Enterprise Plus
 drush config-set system.theme default enterpriseplus -y
+
+#Enable the main menu for the theme.
+drush en mtt_enterpriseplus_theme_mtt_main_navigation -y
 
 echo "Optimize Composer Autoloader"
 cd ${DWD}
