1a1b07e16a5871184c5a563e218a40ed71e55cf2
Brady Owens <info@fastglass.net>
2017-11-27 22:34:25 -0500

    PDS-77 CSFR token cannot be used with this because Views doesn't create the URL with a token. WIP on the controller.

--- modules/custom/pds_create_node_from_webform/src/Controller/CreateNodeController.php
+++ modules/custom/pds_create_node_from_webform/src/Controller/CreateNodeController.php
@@ -3,7 +3,7 @@
 namespace Drupal\pds_create_node_from_webform\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\pds_create_node_from_webform\PDScreateNodeGroupLoader;
+use Drupal\node\Entity\Node;
 
 /**
  * Class CreateNodeController.
@@ -14,9 +14,8 @@ class CreateNodeController extends ControllerBase {
    * Createnode.
    *
    * @return array
-   *   Return Hello string.
    */
-  public function createNodePage() {
+  public function createNodePage($webformid) {
     // Call the service to get group membership information.
     $groupMemmbership = \Drupal::service('group.membership_loader');
     // Get the array of group memberships for the currentUser.
@@ -24,13 +23,21 @@ class CreateNodeController extends ControllerBase {
     // Array for group IDs.
     $gid = [];
     foreach ($access as $membership) {
-      $single = $membership->getGroup();
-      $gid[] = $single->id();
+      $gid[] = $membership->getGroup()->id();
     }
+
+    $form = Node::load($webformid);
+
+    // Load all Webforms.
+    // $nids = \Drupal::entityQuery('node')->condition('type','webform')->execute();
+
+    $accessCheckService = \Drupal::service('access_check.group.owns_content');
+
+
     $stuff = 1;
     return [
       '#type' => 'markup',
-      '#markup' => $this->t('Your Membership is: ' . print_r($gid)),
+      '#markup' => $this->t('Your Membership is: '),
     ];
   }
 }
