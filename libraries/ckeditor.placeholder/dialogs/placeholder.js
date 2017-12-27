f4a9dd73931d9b6e570a222fd7f9542751bb64eb
Brady Owens <info@fastglass.net>
2017-12-27 18:21:26 -0500

    PDS-64 Reformatted JS file to be spaces instead of tabs. This now pulls the tokens from the JS variable.

--- libraries/ckeditor.placeholder/dialogs/placeholder.js
+++ libraries/ckeditor.placeholder/dialogs/placeholder.js
@@ -1,4 +1,3 @@
-
 /**
  * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
@@ -11,51 +10,51 @@
 
 'use strict';
 
-CKEDITOR.dialog.add( 'placeholder', function( editor ) {
-	var lang = editor.lang.placeholder,
-		generalLabel = editor.lang.common.generalTab,
-		validNameRegex = /^[^\[\]<>]+$/;
+CKEDITOR.dialog.add('placeholder', function (editor) {
+  var lang = editor.lang.placeholder,
+    generalLabel = editor.lang.common.generalTab,
+    validNameRegex = /^[^\[\]<>]+$/;
 
   //  TODO - Need to finish the integration code after we decide on the specific Document content type structure.
-	var myString = document.getElementById('available_tokens').innerText;
-	var temparray = myString.split(",");
+  var elements = new Array();
+  elements = drupalSettings.ckeditoraddinplaceholder.questionTokens;
   var arr = new Array();
 
-  for (var i = 0; i < temparray.length; i++) {
+  for (var i = 0; i < elements.length; i++) {
     arr[i] = new Array();
-    arr[i][0] = temparray[i];
+    arr[i][0] = elements[i];
   }
 
-	return {
-		title: lang.title,
-		minWidth: 300,
-		minHeight: 80,
-		contents: [
-			{
-				id: 'info',
-				label: generalLabel,
-				title: generalLabel,
-				elements: [
-					// Dialog window UI elements.
-					{
-						id: 'name',
-						//type: 'text',
+  return {
+    title: lang.title,
+    minWidth: 300,
+    minHeight: 80,
+    contents: [
+      {
+        id: 'info',
+        label: generalLabel,
+        title: generalLabel,
+        elements: [
+          // Dialog window UI elements.
+          {
+            id: 'name',
+            //type: 'text',
             type: 'select',
             items: arr,
-						style: 'width: 400px;',
-						label: lang.name,
-						'default': '',
-						required: true,
-						validate: CKEDITOR.dialog.validate.regex( validNameRegex, lang.invalidName ),
-						setup: function( widget ) {
-							this.setValue( widget.data.name );
-						},
-						commit: function( widget ) {
-							widget.setData( 'name', this.getValue() );
-						}
-					}
-				]
-			}
-		]
-	};
-} );
+            style: 'width: 400px;',
+            label: lang.name,
+            'default': '',
+            required: true,
+            validate: CKEDITOR.dialog.validate.regex(validNameRegex, lang.invalidName),
+            setup: function (widget) {
+              this.setValue(widget.data.name);
+            },
+            commit: function (widget) {
+              widget.setData('name', this.getValue());
+            }
+          }
+        ]
+      }
+    ]
+  };
+});
