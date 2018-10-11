2440f419ec83218b6d41cbbb0f628b1451ed8a9f
Scott Weiss <scott@fastglass.net>
2018-10-11 10:52:42 -0500

    PDS-189 - Updating Taxonomy export script for email template options.

--- 5Migration/data_export_taxonomy.sh
+++ 5Migration/data_export_taxonomy.sh
@@ -10,4 +10,8 @@ drush dce taxonomy_term 2 --file=$parentdir/modules/custom/pds_configuration_bas
 drush dce taxonomy_term 3 --file=$parentdir/modules/custom/pds_configuration_base_data/content/taxonomy_term/pds_instruction_template_type3.json
 drush dce taxonomy_term 4 --file=$parentdir/modules/custom/pds_configuration_base_data/content/taxonomy_term/pds_instruction_template_type4.json
 drush dce taxonomy_term 5 --file=$parentdir/modules/custom/pds_configuration_base_data/content/taxonomy_term/pds_instruction_template_type5.json
+drush dce taxonomy_term 6 --file=$parentdir/modules/custom/pds_configuration_base_data/content/taxonomy_term/pds_email_template_type6.json
+drush dce taxonomy_term 7 --file=$parentdir/modules/custom/pds_configuration_base_data/content/taxonomy_term/pds_email_template_type7.json
+drush dce taxonomy_term 8 --file=$parentdir/modules/custom/pds_configuration_base_data/content/taxonomy_term/pds_email_template_type8.json
+drush dce taxonomy_term 9 --file=$parentdir/modules/custom/pds_configuration_base_data/content/taxonomy_term/pds_email_template_type9.json
 echo Completed Node Export!!!
